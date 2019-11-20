f9dfe35b99c9d7b7a24b8329845ea7869f2e6717
DanBaisal <dan.baisal92@gmail.com>
2019-11-21 00:21:21 +0100

    Added a Javascript file

---
+++ script.js
@@ -0,0 +1,48 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('Start');
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setQuestion()
+}
+
+function setQuestion() {
+    showQuestions(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestions(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+
+    })
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: 'What is 2+2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    }
+]
